82f381af2572c1087edc7b65bd636881ef720eb7
Shubham Sawant <89156287+sawantshubham086@users.noreply.github.com>
2022-02-13 12:36:59 +0530

    Add files via upload

---
+++ CLIENT.sql
@@ -0,0 +1,19 @@
+CREATE TABLE CLIENT (CLIENT_ID NUMBER(10)CONSTRAINTS PK_CL_ID PRIMARY KEY,
+FIRST_NAME VARCHAR2(15),LAST_NAME VARCHAR2(15),CLIENT_MAIL VARCHAR2(30),
+CLIENT_PHONE NUMBER(15),CLIENT_ADD VARCHAR2(25),CLIENT_CITY VARCHAR2(20))
+
+INSERT INTO CLIENT VALUES(1,'Yogesh','Walake','walake1@gamil.com','8793927151','Hadapsar','pune')
+INSERT INTO CLIENT VALUES(2,'Rushikesh','Ghorapade','ghorapade2@gamil.com','7057219829','Phursungi','pune')
+INSERT INTO CLIENT VALUES(3,'Pritam','Kokare','kokare3@gamil.com','8888168381','karamala','solapur')
+INSERT INTO CLIENT VALUES(4,'Gokul','Chavan','chavan4@gamil.com','9527117402','apti','solapur')
+INSERT INTO CLIENT VALUES(5,'Omkar','Chaskar','chaskar5@gamil.com','9730981346','annapur','shirur')
+INSERT INTO CLIENT VALUES(6,'Shubham','Jejurkar','jejurkar6@gamil.com','8482930478','kuruli','shirur')
+INSERT INTO CLIENT VALUES(7,'Nihal','Jagtap','jagtap7@gamil.com','7071710016','sasanenagar','pune')
+INSERT INTO CLIENT VALUES(8,'Praful','Tayade','tayade8@gamil.com','7385377397','akurdi','pune')
+INSERT INTO CLIENT VALUES(9,'Pruthviraj','Girme','girme@gamil.com','9503927514','hinjewadi','pune')
+INSERT INTO CLIENT VALUES(10,'Vikrant','Ghanavat','ghanavat10@gamil.com','7499992941','manjari','pune')
+INSERT INTO CLIENT VALUES(11,'Shubham','Thorat','thorat11@gamil.com','9822081370','kalepadal','pune')
+INSERT INTO CLIENT VALUES(12,'Machindra','Kale','kale12@gamil.com','8379878988','saswad','pune')
+INSERT INTO CLIENT VALUES(13,'Wasim','Khan','Khan13@gamil.com','8379878988','alur','banglore')
+INSERT INTO CLIENT VALUES(14,'Akash','Pawar','pawar14@gamil.com','9011681040','nampally','hydrabad')
+INSERT INTO CLIENT VALUES(15,'Yogesh','Jadhav','jadhav15@gamil.com',null,null,null)
